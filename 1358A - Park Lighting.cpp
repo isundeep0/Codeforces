cfc6de87a0bfb16ca9d6a576678fae9febd2be20
Sandeep <isundeep0@gmail.com>
2023-12-15 22:01:28 +0530

    math6 & 7

---
+++ 1358A - Park Lighting.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+  int t;
+  cin>>t;
+
+  while (t--){
+    int n, m;
+
+    cin>>n>>m;
+
+    cout<<(int)ceil(n*m/2.0)<<endl;
+
+
+
+  }
+
+  return 0;
+}
