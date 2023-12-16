61dfbe21ac1266fdb3acbbf8f4adeeb157a76dd9
Sandeep <isundeep0@gmail.com>
2023-12-16 12:23:24 +0530

    math12

--- 1367B - Even Array.cpp
+++ 1367B - Even Array.cpp
@@ -8,13 +8,9 @@ int main() {
 
   while (t--){
     int n;
-
     cin>>n;
 
-    
-
     int oddCount = 0, evenCount = 0, moves = 0;
-
 
     for(int i = 0; i<n; i++){
       int a;
@@ -42,8 +38,6 @@ int main() {
     else{
       cout<<-1<<endl;
     }
-
-
 
   }
 
