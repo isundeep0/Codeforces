164cdd8187db275356aa3a854889c382387ddba4
Sandeep <isundeep0@gmail.com>
2023-12-17 11:43:11 +0530

    math23

---
+++ 1195A - Drinks Choosing.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+
+      int n, k;
+
+      cin>>n>>k;
+
+      vector<int> v(k+1, 0);
+
+      int ans = 0;
+
+      for (int i = 0; i<n; i++){
+        int a;
+        cin>>a;
+        v[a]++;
+        if (v[a] == 2){
+          ans += v[a];
+          v[a] = 0;
+        }
+      }
+
+
+      int cnt = 0;
+
+      for (int i = 0; i<=k; i++){
+        if (v[i] > 0){
+          ++cnt;
+        }
+      }
+
+      ans += ceil((double)cnt/2);
+
+      cout<<ans<<endl;
+      
+    
+  return 0;
+}
+
